ce2d1b766ab4e633d7c358bfc04b5480643ee8ef
YOSHITH <y131141191@gmail.com>
2026-09-12 12:57:46 +0530

    Implement temperature conversion from Fahrenheit to Celsius
    Add a program to convert Fahrenheit to Celsius.

--- first-year-week-2-assignment-q-1.c
+++ first-year-week-2-assignment-q-1.c
@@ -1,2 +1,11 @@
-# C-assignments
-Submitting assignments
+#include<stdio.h>
+int main(){ 
+    printf("Enter temperature in Fahrenheit:");
+    float a;
+    scanf("%f",&a);
+    float b=(a-32)*5/9;
+    printf("---conversion result---\n");
+    printf("Fahrenheit:%.2f\n",a);
+    printf("celsius:%.2f",b);
+    return 0;
+}
